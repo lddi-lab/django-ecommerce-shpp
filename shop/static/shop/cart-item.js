ba592a321c4fada43b60eb8377bef3d6653a0492
Lucky Icban <lddicban@gmail.com>
2023-07-15 13:28:44 +0800

    calculate order total

--- shop/static/shop/cart-item.js
+++ shop/static/shop/cart-item.js
@@ -5,13 +5,19 @@ else {
     cart = JSON.parse(localStorage.getItem('cart'));
 }
 
+var total = 0;
 for (item in cart){
     let name = cart[item][1];
     let quantity = cart[item][0];
+    let price = cart[item][2];
+    total = total + cart[item][2];
 
-    itemString = `<li class="list-group-item d-flex justify-content-between align-items-center">${name}<span class="badge bg-primary rounded-pill">${quantity}</span></li>`;
+
+    itemString = `<li class="list-group-item d-flex justify-content-between align-items-center"><span class="badge bg-primary rounded-pill">${quantity}</span> ${name} <span class="badge bg-warning rounded-pill" >$ ${price}</span></li>`;
     $('#item_list').append(itemString);
-
 }
+
+var totalPrice = `<li class="list-group-item d-flex justify-content-between aligh-items-center"><b>Your total <span class="badge bg-danger rounded-pill" >$${ total }</span></b></li>`;
+$('#item_list').append(totalPrice);
 
 $('#items').val(JSON.stringify(cart));
