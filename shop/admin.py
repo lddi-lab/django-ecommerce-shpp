76ac2f44c8f7bc5ebee1d0ee2c058a0d739edc3f
Lucky Icban <lddicban@gmail.com>
2023-07-17 22:12:15 +0800

    admin panel customisation - updated headers

--- shop/admin.py
+++ shop/admin.py
@@ -3,5 +3,10 @@ from .models import Products, Order
 
 
 # Register your models here.
+
+admin.site.site_header = "E-commerce Site"
+admin.site.site_title = "ABC Shopping"
+admin.site.index_title = "Manage ABC Shopping"
+
 admin.site.register(Products)
 admin.site.register(Order)
